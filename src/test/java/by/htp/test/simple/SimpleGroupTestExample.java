f2a8563308f61bb64987baf57c2988f58dbe7c08
HTPTAT7 <36242179+HTPTAT7@users.noreply.github.com>
2018-07-17 13:17:43 +0300

    Add files via upload

---
+++ src/test/java/by/htp/test/simple/SimpleGroupTestExample.java
@@ -0,0 +1,23 @@
+package by.htp.test.simple;
+
+import org.testng.annotations.Test;
+
+public class SimpleGroupTestExample {
+	
+	
+	@Test(groups = "simple")
+	public void testOne() {
+		System.out.println("one");
+	}
+	
+	@Test(groups = {"simple", "complex"})
+	public void testTwo() {
+		System.out.println("two");
+	}
+	
+	@Test(groups = {"simple", "complex"})
+	public void testThree() {
+		System.out.println("three");
+	}
+
+}
